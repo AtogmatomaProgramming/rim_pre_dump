31a725739fd1dba30feae992a3c5ad9fbaef2fef
Eucrow <moko@sierpe.org>
2016-08-19 15:07:56 +0200

    Change to use package sapmubase

--- IPDtoSIRENO.R
+++ IPDtoSIRENO.R
@@ -5,16 +5,24 @@
 ####
 #### author: Marco A. Amez Fernandez
 #### email: ieo.marco.a.amez@gmail.com
-#### date of last modification: 1/7//2016
+#### date of last modification: 22/7//2016
 #### version: 0.1
 ####
-#### files required:
+#### files required: metiers.csv
 
 
 # #### CONFIG ##################################################################
 
 # ---- PACKAGES ----------------------------------------------------------------
 
+library(dplyr) #arrange_()
+library(devtools) # Need this package to use install_github
+install_github("Eucrow/sapmuebase") # Make sure this is the last version
+library(sapmuebase) # and load the library
+
+#initial_wd <- getwd()
+#setwd("F:/misdoc/sap")
+#install("sapmuebase")
 
 # ---- SET WORKING DIRECTORY ---------------------------------------------------
 
@@ -39,57 +47,25 @@ YEAR <- "2016"
 
 PATH_ERRORS <- paste(PATH_FILENAME,"/errors",sep="")
 
+# #### IMPORT FILES ############################################################
+metiers <- read.csv("metiers_nuevo.csv")
+puertos <- read.csv("puerto_locode_sireno.csv")
+divisiones <- read.csv("divisiones.csv")
+
+# #### CONSTANS ################################################################
+
+###list with the common fields used in the tables
+BASE_FIELDS <- c("PUERTO", "FECHA", "BARCO", "UNIPESCOD", "TIPO_MUESTREO")
+
 # #### FUNCTIONS ###############################################################
 
-#function to save the errors in csv files:
-export_errors_lapply<-function(x, errors){
-  if(nrow(errors[[x]])!= 0){
-    fullpath<-paste(PATH_ERRORS, "/", YEAR, "_", MONTH, "_errors_", x, ".csv", sep="")
-    write.csv(errors[[x]], file=fullpath, row.names = FALSE, quote = FALSE)
-    MESSAGE_ERRORS[[x]]<<-"has errors" #this not recomended in R but is the only way I know
-    print(x)
-  } else {
-    fullpath<-paste(PATH_ERRORS, "/", YEAR, "_", MONTH, "_no_errors_", x, ".csv", sep="")
-    write.csv(errors[[x]], file=fullpath, row.names = FALSE, quote = FALSE)
-    MESSAGE_ERRORS[[x]]<<-"errors free" #this not recomended in R but is the only way I know
-    print(paste('Great,', x, 'is error free!'))
-  }
-}
 
+# ---- import file ---- #
+records <- importIPDFile("data/muestreos_especie_4_2016_todos_.txt")
 
-#function to import and clean the IPD file
-##filename: name to import
-##export = "TRUE" the file is export to csc
-importIPDFile <- function(){
-  
-  fullpath<-paste(PATH_FILENAME, FILENAME, sep="/")
-  
-  #read the file
-  records <- read.fwf(
-    file="data/muestreos_especie_1_2016_todos.txt",
-    widths=c(10, 4, 6, 3, 3, 21, 10, 20, 1, 10, 8,  7, 7, 4, 20, 5, 20, 10, 4, 20, 5, 1, 20, 20, 10, 10, 10)
-  )
-  
-  colnames(records) <- c("fecha_muestreo", "cod_puerto", "cod_buque", "cod_arte",
-                      "cod_origen", "metier", "cod_proyecto", "tipo_muestreo",
-                      "numero_rechazados", "numero_barcos_muestreados", "cod_cuadricula",
-                      "latitud_decimal", "longitud_decimal", "dias_mar", "peso_total",
-                      "cod_especie_tax", "tipo_muestreo2", "procedencia", "cod_categoria",
-                      "peso", "cod_especie_muestreada", "sexo", "peso_muestra", "medida",
-                      "talla", "numero_ejemplares", "cod_puerto_descarga")
-  
-  #select only samples from ICES
-  records <- records[records$"cod_proyecto"==1101001,]
-  
-  #format fecha_muestreo
-  records$fecha_muestreo <- as.POSIXlt(records$fecha_muestreo, format="%d/%m/%Y")
-
-  #return data
-  if (MONTH != ""){
-    return(subset(records, fecha_muestreo$mon == MONTH-1))
-  } else {
-    return(records)
-  }
-}
-
-records <- importIPDFile()
+# ---- metiers ----#
+levels(records$metier)
+levels(records$tipo_muestreo)
+errors_1_metiers <- merge(x = records, y= metiers, by.x = c("tipo_muestreo", "metier", "cod_puerto", "cod_origen"), by.y = c("tipo_muestreo", "UNIPESCOD", "COD_SIRENO", "COD_DIVISION_SIRENO"), all.x = TRUE)
+errors_1_metiers <- unique(errors_1_metiers$metier)
+nas <- subset(errors_1_metiers, is.na(errors_1_metiers$ARTE))
