e7158bf9b789e8e822f0e729839f1586f8324c7d
Marco A. Ámez <marco.a.amez@gmail.com>
2024-07-04 10:04:14 +0200

    Minor changes.

--- rim_pre_dump.R
+++ rim_pre_dump.R
@@ -56,7 +56,7 @@ YEAR <- "2024"
 
 # Suffix to add to path. Use only in case MONTH is a vector of months. This
 # suffix will be added to the end of the path with a "_" as separation.
-suffix_multiple_months <- "annual_post_oab"
+suffix_multiple_months <- ""
 
 # Suffix to add at the end of the export file name. This suffix will be added to
 # the end of the file name with a "_" as separation.
@@ -117,43 +117,6 @@ file_name <- unlist(strsplit(FILENAME, '.', fixed = T))
 file_name <- paste0(file_name[1], '_raw_imported.csv')
 
 exportCsvSAPMUEBASE(records, file_name, path = PATH_FILES)
-
-
-#' Check code:
-#' Check variable with prescriptions data set. Use the
-#' metier_coherence data set from sapmuebase.
-#' @param df Dataframe where the variable to check is.
-#' @param variable Variable to check as character. Allowed variables:
-#' ESTRATO_RIM, COD_ORIGEN, COD_ARTE, METIER_DCF and CALADERO_DCF.
-#' @return dataframe with errors
-checkVariableWithMetierCoherence <- function(df, variable){
-
-    valid_variables = c("ESTRATO_RIM", "COD_ORIGEN", "COD_ARTE", "METIER_DCF",
-                        "CALADERO_DCF")
-
-    if (!(variable %in% valid_variables)) {
-      stop(paste("This function is not available for variable ", variable))
-    }
-
-    allowed <- sapmuebase::metier_coherence[,variable]
-
-    df <- df[!(df[[variable]] %in% allowed), ]
-
-
-    fields <- BASE_FIELDS
-
-    if (!(variable %in% BASE_FIELDS)) {
-      fields <- c(BASE_FIELDS, variable)
-    }
-
-    df <- df[, fields]
-
-    df <- unique(df)
-
-    return(df)
-
-}
-
 
 
 # START CHECK ------------------------------------------------------------------
